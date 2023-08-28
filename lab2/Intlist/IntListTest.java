5e6d8714f30468fa0b217f71c3712eebe9d7f43a
Kichi <2942387166@qq.com>
2023-08-28 20:54:44 +0800

    completed lab2

--- lab2/Intlist/IntListTest.java
+++ lab2/Intlist/IntListTest.java
@@ -3,14 +3,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class IntListTest {
-
-    /**
+    @Test
+     /**
      * Example test that verifies correctness of the IntList.of static
      * method. The main point of this is to convince you that
      * assertEquals knows how to handle IntLists just fine.
      */
 
-    @Test
     public void testList() {
         IntList one = new IntList(1, null);
         IntList twoOne = new IntList(2, one);
